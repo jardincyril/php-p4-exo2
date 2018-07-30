9ae8aaf5d2f1f4c673407294bf581847d9ed1f19
cyril jardin <cyilroc60@hotmail.fr>
2018-07-30 09:44:25 +0200

    fonction

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo2</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      function melting($string1){
+        $result = $string1;
+        return $result;
+      }
+      echo melting('comment va tu');
+      ?>
+    </p>
+  </body>
+</html>
